53503cabd125a359597fe30cb873c61a29e8b3dc
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-12-08 18:55:41 +0100

    First React

---
+++ React/Intro-to-React/first_project_react_jsx/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title className="App-title">Hello dojo</title>
+          <h1>Hello Dojo!</h1>
+          <h3>Things I need to do :</h3>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt Everest</li>
+            <li>Run a marathan</li>
+            <li>Fedd the dogs</li>
+          </ul>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
