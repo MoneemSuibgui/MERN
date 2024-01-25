b47a3ed8c40cf8ae9cb066d0ab9036a1610fa6dd
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-01-25 20:11:21 +0100

    Import mongoose & connect to db

---
+++ Full_Stack_MERN/Refactor,Reuse,Recycle_Product_Manager_without_node/server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+// import mongoose to connecting with mongo db
+const mongoose=require("mongoose")
+
+// set up monggose.connect function 
+mongoose.connect("mongodb://0.0.0.0/productManager",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log('Established a connection to the database'))
+.catch(err=>console.log('Something went wrong when connecting to the database',err))
